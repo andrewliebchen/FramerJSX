c8744217b58c31e5729833b3385673f90d0abb67
Andrew Liebchen <liebchen@fb.com>
2017-07-12 22:47:31 -0700

    Children WIP

--- src/index.js
+++ src/index.js
@@ -4,22 +4,43 @@ import { render } from 'react-dom';
 // Device types?
 const Prototype = (props) => props.children;
 
-// Layer states?
 class FLayer extends Component {
+	_bindEvents(layer) {
+		const { onClick } = this.props;
+
+		onClick && layer.onClick(() => {
+      onClick(layer);
+    });
+	}
+
+	_registerStates(layer) {
+		const { states } = this.props;
+
+		states && states.map((state, i) => {
+			const stateName = Object.keys(state);
+			layer.states[stateName] = state[stateName];
+		});
+	}
+
+	_renderChildren(layer) {
+		const { children } = this.props;
+
+		React.Children.forEach(children, (child) => {
+			console.log(child);
+    });
+	}
+
+	_constructLayerAccessories(layer) {
+		this._bindEvents(layer);
+		this._registerStates(layer);
+    this._renderChildren(layer);
+	}
+
   render() {
+		// Render parent layer
     let parentLayer = new Layer(this.props);
 
-    parentLayer.onClick(() => {
-      console.log(parentLayer.states);
-      this.props.onClick(parentLayer);
-    });
-
-    this.props.children.length > 1 && this.props.children.map((child, i) =>
-      new Layer(Object.assign(
-        { parent: parentLayer },
-        child.props
-      ))
-    );
+		this._constructLayerAccessories(parentLayer);
 
     return false;
   }
@@ -36,14 +57,14 @@ render(
     <F.Layer
       backgroundColor={new Color('blue').alpha(0.5)}
       x={Align.center(20)}
-      y={Align.center(20)}
-      states={{ hide: { opacity: 0 }}}
-      onClick={(self) => { self.stateCycle() }}>
+      y={Align.center(20)}>
 
       <F.Layer
         backgroundColor={new Color('red').alpha(0.5)}
         x={Align.center(-20)}
-        y={Align.center(-20)}/>
+        y={Align.center(-20)}
+				states={[{ hide: { opacity: 0 }}]}
+	      onClick={(self) => { self.stateCycle() }}/>
 
       <F.Layer
         backgroundColor={new Color('magenta').alpha(0.5)}
